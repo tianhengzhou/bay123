df641d950e68f4767abb2b71679d56d1e664cb0e
Tianheng Zhou <tianhengzhou@TianhengtekiMacBook-Air.local>
2017-07-09 00:07:52 -0700

    For guanheng's autopost

--- bay123.py
+++ bay123.py
@@ -1,22 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import time
 from selenium import webdriver
-# from urlparse import urlparse
-# from urlparse import parse_qs
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.common.exceptions import NoSuchElementException
-# from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.alert import Alert
-# from selenium.webdriver.support.ui import Select
-# import re
-# from retrying import retry
 
 
 class Modules:
     def __init__(self):
-        self.driver = webdriver.Chrome('chromedriver.exe')
+        self.driver = webdriver.Chrome()
 
     def bay123_autopost(self):
         url = 'http://bay123.com/forum.php?mod=post&action=newthread&fid=40'
@@ -25,11 +19,12 @@ class Modules:
         self.driver.find_element_by_id('typeid_ctrl').click()
         south_bay_xpath = "//div[@id='typeid_ctrl_menu']/ul/li[2]"
         self.driver.find_element_by_xpath(south_bay_xpath).click()
-        self.driver.find_element_by_id('subject').send_keys('南湾 95123 客房出租 $1200  带家具包水电拎包入住')
-        self.driver.find_element_by_id('e_textarea').send_keys('南湾 95123 出租一间客房 $1200 带家具包水电费 拎包入住。' +
-                                                               '小区自带健身房，游泳池。步行距离至超市。2mile 到' +
-                                                               'costco，walmart，whole food。交通便利，离85和101很近。' +
-                                                               '\nhttp://bay123.com/thread-33029-1-1.html')
+        self.driver.find_element_by_id('subject').send_keys(u'Cupertino 2b1b apartment 主卧出租 $1400 不包水电')
+        self.driver.find_element_by_id('e_textarea').send_keys(u'Cupertino 2b1b apartment 主卧出租 $1400 不包水电。' +
+                                                               u'小区位置超好，3分钟到safeway， target，鲜芋仙，cupertino main stree ' + 
+                                                               u'5分钟到大华99，附近超多中餐馆，离苹果和De Anza College几分钟，出门就能上280。' + 
+                                                               u'面积825 sqft，超大主卧，和室友共用bathroom，要求男性，爱干净，好相处。\n' + 
+                                                               u'联系方式： 5416027889（text only）')
         self.driver.find_element_by_xpath("//div[@class='mtm']/span/span/input").send_keys('2017')
         self.driver.find_element_by_id('postsubmit').click()
         time.sleep(2)
